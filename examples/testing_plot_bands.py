9bc94eaaa95cdf375b6cfd4e590b747a5f148595
cardila86 <carlosardilae@gmai.com>
2025-02-06 10:02:43 -0500

    vaspkit bands plotting now is general band plotting. For now, it works for vaspkit and wannier90. Example is included. Also, implementation of band plotting with atoms, spin and orb projection using pyprocar

--- examples/testing_plot_bands.py
+++ examples/testing_plot_bands.py
@@ -1,28 +1,44 @@
 from plotting_class import plottingTools
+import matplotlib.pyplot as plt
+
 #from plotting_class import styler
 import time
 
 t0 = time.time()
 
 plotter = plottingTools()
+fig, ax = plt.subplots()
 
-fig, ax = plotter.plot_bands_vaspkit(path_read='/home/carlos/Documents/scriptsCalculations/projects/RPt2B/NdPt2B/FM001/k131311/2-bands-SOC',
+
+path_read_vaspkit = '/home/carlos/Documents/scriptsCalculations/projects/RPt2B/NdPt2B/FM001/k131311/2-bands-SOC-2'
+path_read_wannier90 = '/home/carlos/Documents/scriptsCalculations/projects/RPt2B/NdPt2B/FM001/k11119/3-wannier-SOC'
+ax = plotter.plot_bands(path_read_vaspkit,
                                     E_limit=[-1, 1],
+                                    program='vaspkit',
                                     E_zero=8.1944,
                                     E_vaspkit=False,
                                     klabels=[r'$\Gamma$','M','K',r'$\Gamma$','A','L','H','A|L',
-                                            'M|H', r'$K|\Gamma$','M\'','K\'',r'$\Gamma$', 'A\'',
-                                            'L\'', 'H\'', 'A\'|L\'','M\'|H\'','K\''],
+                                            'M|H', r'$K'],
                                     kticks= None,
                                     kbreaks=None,
                                     label=None,
                                     color='k',
-                                    ax=None,
+                                    ax=ax,
+                                    show=False,
+                                    savefile=None)
+ax = plotter.plot_bands(path_read_wannier90,
+                                    E_limit=[-1, 1],
+                                    program='wannier90',
+                                    E_zero=8.1944,
+                                    E_vaspkit=False,
+                                    root='wannier90',
+                                    klabels=None,
+                                    kticks= None,
+                                    kbreaks=None,
+                                    label=None,
+                                    color='r',
+                                    ax=ax,
                                     show=True,
                                     savefile=None)
-
-
-#bands = styler(fig, ax)
-# bands.bandstructure_style(savefile='./testing/bands_updw/bands')
 
 print('Elapsed time: ', time.time()-t0)
